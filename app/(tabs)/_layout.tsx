267e43815befd9acefd567fc992870669bd57b03
Musayeva Munavvar Islam <Musa_qo76@itstep.edu.az>
2025-07-24 15:48:15 +0400

    first commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -40,6 +40,15 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
+
+<Tabs.Screen
+        name="info"
+        options={{
+          href:"/(tabs)/info",
+          title: 'Info',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+        }}
+      />
     </Tabs>
   );
 }
